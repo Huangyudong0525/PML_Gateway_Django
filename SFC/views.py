959cff6013ddb46599d4c79829288e8bb0cbb36f
Huangyudong0525 <hyduni@163.com>
2019-10-25 19:37:23 +0800

    修复了nf_router_conf LIST_SIZE的bug

--- SFC/views.py
+++ SFC/views.py
@@ -9,8 +9,14 @@ import time
 import signal
 import psutil
 import json
+import paramiko
 
 processes = dict()
+
+#解决sudo权限
+#sudopw = "123"
+#cmd = " "
+#os.system("echo %s |sudo -S %s"%(sudopw, cmd))
 
 def index(request):
     return render(request, "index.html",)
@@ -19,35 +25,36 @@ def killProc(pid):
     baseProc = psutil.Process(pid)   #读取进程
     childrenProcList = baseProc.children(recursive=True)  #递归读取进程的子进程树
     for proc in childrenProcList:
-        os.kill(proc.pid, signal.SIGINT)    #杀死子进程
+        os.system('sudo kill -2 %d'%proc.pid)
+        #os.kill(proc.pid, signal.SIGINT)    #杀死子进程
         # if (len(proc.children(recursive=True)) == 0):
         # os.kill(pid, signal.SIGINT)
         # else:
         # killProc(proc.pid)
 
 def choose_nf(nf_name, service_id, nexthop_id):
-    cmd1 = "cd /home/hyd/openNetVM/examples/%s ; ./go.sh %d -d %d " % (nf_name, service_id, nexthop_id)
+    cmd1 = "cd /home/hyd/openNetVM/examples/%s ; sudo ./go.sh %s -d %s " % (nf_name, service_id, nexthop_id)
     devnull = open('/dev/null', 'w')
     p = subprocess.Popen(cmd1, stdout=devnull, shell=True)
     print("nf ok")
     return p.pid
 
 def choose_nf_router(service_id):
-    cmd1 = "cd /home/hyd/openNetVM/examples/nf_router ; ./go.sh %d  -f ./route.conf " % (service_id)
+    cmd1 = "cd /home/hyd/openNetVM/examples/nf_router ; sudo ./go.sh %s -f ./route.conf " % (service_id)
     devnull = open('/dev/null', 'w')
     p = subprocess.Popen(cmd1, stdout=devnull, shell=True)
     print("nf_router ok")
     return p.pid
 
 def choose_firewall(service_id, nexthop_id):
-    cmd1 = "cd /home/hyd/openNetVM/examples/firewall ; ./go.sh %d -d %d -f ./rules.json " % (service_id, nexthop_id)
+    cmd1 = "cd /home/hyd/openNetVM/examples/firewall ; sudo ./go.sh %s -d %s -f ./rules.json " % (service_id, nexthop_id)
     devnull = open('/dev/null', 'w')
     p = subprocess.Popen(cmd1, stdout=devnull, shell=True)
     print("firewall ok")
     return p.pid
 
 def choose_bridge(service_id):
-    cmd1 = "cd /home/hyd/openNetVM/examples/bridge ; ./go.sh %d " % (service_id)
+    cmd1 = "cd /home/hyd/openNetVM/examples/bridge ; sudo ./go.sh %s " % (service_id)
     devnull = open('/dev/null', 'w')
     p = subprocess.Popen(cmd1, stdout=devnull, shell=True)
     print("bridge ok")
@@ -105,6 +112,7 @@ def read_nf_router_conf(request):
     rules_list2 = []
     rules_list3 = []
     rules_container = []
+    #with open('/home/hyd/openNetVM/examples/nf_router/route.conf', "r", encoding='utf-8') as fid:
     with open('C:/Users/HYD/PycharmProjects/PML_Security_Gateway/SFC/route.conf', "r", encoding='utf-8') as fid:
         for line in fid.readlines()[1:]:    #将文件按行读取为列表
             num = line.split(' ')           #每行用空格分隔为两部分，分别用列表存
@@ -123,7 +131,8 @@ def read_nf_router_conf(request):
 
 def read_firewall_conf(request):
     rules_container = []
-    with open('C:/Users/HYD/PycharmProjects/PML_Security_Gateway/SFC/rules.json', "r", encoding='utf-8') as fid:
+    with open('/home/hyd/openNetVM/examples/firewall/rules.json', "r", encoding='utf-8') as fid:
+    #with open('C:/Users/HYD/PycharmProjects/PML_Security_Gateway/SFC/rules.json', "r", encoding='utf-8') as fid:
         rules_dict = json.load(fid)    #将json文件读取并转换为字典
     key_list =list(rules_dict.keys())     #将字典的key值存进列表
     for key in range(len(key_list)):
@@ -131,35 +140,68 @@ def read_firewall_conf(request):
         rules_dict1['rule_name'] = key_list[key]
         rules_dict1['src_ip'] = rules_dict[key_list[key]]['ip']
         rules_dict1['depth'] = rules_dict[key_list[key]]['depth']
-        rules_dict1['action'] = rules_dict[key_list[key]]['action']
+        rules_dict1['action'] = str(rules_dict[key_list[key]]['action'])
         rules_container.append(rules_dict1)        #将字典装进列表
     return JsonResponse(rules_container, safe = False)
 
 
 def nf_router_conf(request):
+    rules_list1 = []
+    rules_list2 = []
+    rules_list3 = []
     dst_ip = request.POST.get('dst_ip')
     to_service_Id = request.POST.get('to_service_Id')
+    # with open('/home/hyd/openNetVM/examples/nf_router/route.conf', "a", encoding='utf-8') as fid:
     with open('C:/Users/HYD/PycharmProjects/PML_Security_Gateway/SFC/route.conf', 'a', encoding='utf-8') as fid:
         fid.write('%s %s\n'%(dst_ip, to_service_Id))
+    # with open('/home/hyd/openNetVM/examples/nf_router/route.conf', "r", encoding='utf-8') as fid:
+    with open('C:/Users/HYD/PycharmProjects/PML_Security_Gateway/SFC/route.conf', "r", encoding='utf-8') as fid:
+        for line in fid.readlines():  # 将文件按行读取为列表
+            num = line.split(' ')  # 每行用空格分隔为两部分，分别用列表存
+            rules_list1.append(num[0])
+            rules_list2.append(num[1])
+    rules_list3 = [x.strip() for x in rules_list2 if x.strip() != '']  # 删除列表中的换行符
+    rules_list3[0] = int(rules_list3[0])+1
+    # with open('/home/hyd/openNetVM/examples/nf_router/route.conf', "w", encoding='utf-8') as fid:
+    with open('C:/Users/HYD/PycharmProjects/PML_Security_Gateway/SFC/route.conf', "w", encoding='utf-8') as fid:
+        for k in range(len(rules_list1)):
+            fid.write('%s %s\n'%(rules_list1[k], rules_list3[k]))
+    # with open('/home/hyd/openNetVM/examples/nf_router/route.conf', "r", encoding='utf-8') as fid:
     with open('C:/Users/HYD/PycharmProjects/PML_Security_Gateway/SFC/route.conf', 'r', encoding='utf-8') as fid:
-    # with open('/home/hyd/openNetVM/examples/nf_router/router.conf', 'w') as fid:
         rules_list = fid.readlines()[1:]
     result = {"Result": "success", "Message": "添加路由规则成功"}
     result["data"] = rules_list
     return JsonResponse(result)
 
 def del_nf_router_conf(request):
+    rules_list1 = []
+    rules_list2 = []
+    rules_list3 = []
     dst_ip = request.POST.get('dst_ip')
+    #with open('/home/hyd/openNetVM/examples/nf_router/route.conf', 'r', encoding='utf-8') as fid:
     with open('C:/Users/HYD/PycharmProjects/PML_Security_Gateway/SFC/route.conf', 'r', encoding='utf-8') as fid:
-    # with open('/home/hyd/openNetVM/examples/nf_router/router.conf', 'w') as fid:
         rules_list = fid.readlines()
+    #with open('/home/hyd/openNetVM/examples/nf_router/route.conf', 'w', encoding='utf-8') as fid:
     with open('C:/Users/HYD/PycharmProjects/PML_Security_Gateway/SFC/route.conf', 'w', encoding='utf-8') as fid:
         for line in rules_list:
             if dst_ip in line:
                 del line
             else:
                 fid.write(line)
+    # with open('/home/hyd/openNetVM/examples/nf_router/route.conf', 'r', encoding='utf-8') as fid:
+    with open('C:/Users/HYD/PycharmProjects/PML_Security_Gateway/SFC/route.conf', "r", encoding='utf-8') as fid:
+        for line in fid.readlines():  # 将文件按行读取为列表
+            num = line.split(' ')  # 每行用空格分隔为两部分，分别用列表存
+            rules_list1.append(num[0])
+            rules_list2.append(num[1])
+    rules_list3 = [x.strip() for x in rules_list2 if x.strip() != '']  # 删除列表中的换行符
+    rules_list3[0] = int(rules_list3[0])-1
+    # with open('/home/hyd/openNetVM/examples/nf_router/route.conf', 'w', encoding='utf-8') as fid:
+    with open('C:/Users/HYD/PycharmProjects/PML_Security_Gateway/SFC/route.conf', "w", encoding='utf-8') as fid:
+        for k in range(len(rules_list1)):
+            fid.write('%s %s\n'%(rules_list1[k], rules_list3[k]))
     result = {"Result": "success", "Message": "删除路由规则成功"}
+    #with open('/home/hyd/openNetVM/examples/nf_router/route.conf', 'r', encoding='utf-8') as fid:
     with open('C:/Users/HYD/PycharmProjects/PML_Security_Gateway/SFC/route.conf', 'r', encoding='utf-8') as fid:
         rules_list = fid.readlines()
     result['data'] = rules_list
@@ -170,16 +212,16 @@ def firewall_conf(request):
     rule_name = request.POST.get('rule_name')
     src_ip = request.POST.get('src_ip')
     depth = request.POST.get('depth')
-    action = request.POST.get('action')
-    with open('C:/Users/HYD/PycharmProjects/PML_Security_Gateway/SFC/rules.json', 'r', encoding='utf-8') as fid:
-    #with open('/home/hyd/openNetVM/examples/firewall/rules.json', 'r') as fid:
+    action = int(request.POST.get('action'))
+    with open('/home/hyd/openNetVM/examples/firewall/rules.json', 'r') as fid:
+    #with open('C:/Users/HYD/PycharmProjects/PML_Security_Gateway/SFC/rules.json', 'r', encoding='utf-8') as fid:
         rules_dict = json.load(fid)
     rules_dict[rule_name] = {}
     rules_dict[rule_name]['ip'] = src_ip
     rules_dict[rule_name]['depth'] = depth
     rules_dict[rule_name]['action'] = action
-    with open('C:/Users/HYD/PycharmProjects/PML_Security_Gateway/SFC/rules.json', "w", encoding='utf-8') as fid:
-    # with open('/home/hyd/openNetVM/examples/firewall/rules.json', "w") as fid:
+    with open('/home/hyd/openNetVM/examples/firewall/rules.json', "w") as fid:
+    #with open('C:/Users/HYD/PycharmProjects/PML_Security_Gateway/SFC/rules.json', "w", encoding='utf-8') as fid:
         json.dump(rules_dict, fid)
     result = {"Result": "success", "Message": "添加防火墙规则成功"}
     result["data"] = rules_dict
@@ -187,10 +229,12 @@ def firewall_conf(request):
 
 def del_firewall_conf(request):
     rule_name = request.POST.get('rule_name')
-    with open('C:/Users/HYD/PycharmProjects/PML_Security_Gateway/SFC/rules.json', "r") as fid:
+    with open('/home/hyd/openNetVM/examples/firewall/rules.json', 'r') as fid:
+    #with open('C:/Users/HYD/PycharmProjects/PML_Security_Gateway/SFC/rules.json', "r") as fid:
         rules_dict = json.load(fid)
     del rules_dict[rule_name]
-    with open('C:/Users/HYD/PycharmProjects/PML_Security_Gateway/SFC/rules.json', "w") as fid:
+    with open('/home/hyd/openNetVM/examples/firewall/rules.json', "w") as fid:
+    #with open('C:/Users/HYD/PycharmProjects/PML_Security_Gateway/SFC/rules.json', "w") as fid:
         json.dump(rules_dict, fid)
     result = {"Result": "success", "Message": "删除防火墙规则成功"}
     result["data"] = rules_dict
@@ -218,6 +262,78 @@ def del_firewall_conf(request):
 #             stop_nf(pid)
 #         if ch == 'e':
 #             os._exit(0)
+
+# 配置ACL
+def set_acl(request):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    acl_name = request.POST.get('acl_name')
+    rule = request.POST.get('rule')
+    ssh.connect(hostname = '172.171.15.41', port = 22, username = 'pml', password = '123')
+    chan = ssh.invoke_shell()
+    chan.send('system-view \n' )
+    if rule == "any":
+        chan.send( "acl %s \n" % acl_name)
+        chan.send("rule permit any \n")
+    else:
+        ip = request.POST.get('ip')
+        mask = request.POST.get('mask')
+        chan.send("acl %s \n" % acl_name)
+        chan.send( "rule permit ip source %s %s \n"%(ip, mask))
+    chan.send('quit \n')
+    result = {"Result":"success", "Message":"添加ACL成功"}
+    return HttpResponse(result)
+
+# 删除ACL
+def del_acl(request):
+    acl_name = request.POST.get('acl_name')
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname='172.171.15.41', port=22, username='pml', password='123')
+    chan = ssh.invoke_shell()
+    chan.send('system-view \n')
+    chan.send("undo acl %s \n" % acl_name)
+    chan.send('quit \n')
+    result = {"Result": "success", "Message": "删除ACL成功"}
+    return HttpResponse(result)
+
+# 配置流分类
+def set_classofier(request):
+    in_port = request.POST.get('in_port')
+    acl_name = request.POST.get('acl_name')
+    out_port = request.POST.get('out_port')
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname='172.171.15.41', port=22, username='pml', password='123')
+    chan = ssh.invoke_shell()
+    chan.send('system-view \n')
+    chan.send( "interface gigabitethernet %s \n" % in_port)
+    chan.send( "traffic-redirect inbound acl %s interface gigabitethernet %s \n"%(acl_name, out_port))
+    chan.send('quit \n')
+    result = {"Result": "success", "Message": "添加流分类成功"}
+    return HttpResponse(result)
+
+# 删除流分类
+def del_classifier(request):
+    in_port = request.POST.get('in_port')
+    acl_name = request.POST.get('acl_name')
+    out_port = request.POST.get('out_port')
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname='172.171.15.41', port=22, username='pml', password='123')
+    chan = ssh.invoke_shell()
+    chan.send('system-view \n')
+    chan.send("interface gigabitethernet %s \n" % in_port)
+    chan.send("undo traffic-redirect inbound acl %s interface gigabitethernet %s \n" % (acl_name, out_port))
+    chan.send('quit \n')
+    result = {"Result": "success", "Message": "删除流分类成功"}
+    return HttpResponse(result)
+
+
+
+
+
+
 
 
 
